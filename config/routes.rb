30766152406b6c22f3c94ccfb185284530ffe845
Beth Secor <elizabeth.a.secor@gmail.com>
2016-02-23 22:05:33 -0700

    Add customers relationships

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,10 @@ Rails.application.routes.draw do
         get 'find_all', to: 'find#index'
         get 'random', to: 'random#show'
       end
-      resources :customers, only: [:show, :index]
+      resources :customers, only: [:show, :index] do
+        get '/invoices', to: 'customers/invoices#index'
+        get '/transactions', to: 'customers/transactions#index'
+      end
 
       namespace :items do
         get 'find', to: 'find#show'
