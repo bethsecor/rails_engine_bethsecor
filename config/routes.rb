2ad855189ed2bdf5830a0bd1e9c53a6b3f4df7a3
Beth Secor <elizabeth.a.secor@gmail.com>
2016-02-23 21:39:01 -0700

    Merchant relationship endpoints complete

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,10 @@ Rails.application.routes.draw do
         get 'find_all', to: 'find#index'
         get 'random', to: 'random#show'
       end
-      resources :merchants, only: [:show, :index]
+      resources :merchants, only: [:show, :index] do
+        get '/items', to: 'merchants/items#index'
+        get '/invoices', to: 'merchants/invoices#index'
+      end
 
       namespace :customers do
         get 'find', to: 'find#show'
@@ -21,21 +24,33 @@ Rails.application.routes.draw do
         get 'find_all', to: 'find#index'
         get 'random', to: 'random#show'
       end
-      resources :items, only: [:show, :index]
+      resources :items, only: [:show, :index] do
+        get '/invoice_items', to: 'items/invoice_items#index'
+        get '/merchant', to: 'items/merchant#show'
+      end
 
       namespace :invoices do
         get 'find', to: 'find#show'
         get 'find_all', to: 'find#index'
         get 'random', to: 'random#show'
       end
-      resources :invoices, only: [:show, :index]
+      resources :invoices, only: [:show, :index] do
+        get '/transactions', to: 'invoices/transactions#index'
+        get '/invoice_items', to: 'invoices/invoice_items#index'
+        get '/items', to: 'invoices/items#index'
+        get '/customer', to: 'invoices/customer#show'
+        get '/merchant', to: 'invoices/merchant#show'
+      end
 
       namespace :invoice_items do
         get 'find', to: 'find#show'
         get 'find_all', to: 'find#index'
         get 'random', to: 'random#show'
       end
-      resources :invoice_items, only: [:show, :index]
+      resources :invoice_items, only: [:show, :index] do
+        get '/invoice', to: 'invoice_items/invoice#show'
+        get '/item', to: 'invoice_items/invoice#show'
+      end
 
       namespace :transactions do
         get 'find', to: 'find#show'
