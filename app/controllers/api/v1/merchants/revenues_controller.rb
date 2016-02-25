695dc02226618ac29a7fa253015bd251facacff4
Beth Secor <elizabeth.a.secor@gmail.com>
2016-02-24 17:06:42 -0700

    Items by most revenue endpoint complete

--- app/controllers/api/v1/merchants/revenues_controller.rb
+++ app/controllers/api/v1/merchants/revenues_controller.rb
@@ -2,10 +2,11 @@ class Api::V1::Merchants::RevenuesController < ApplicationController
   respond_to :json
 
   def show
+    merchant = Merchant.find(params[:merchant_id])
     unless params[:date]
-      respond_with( { revenue: Merchant.find(params[:merchant_id]).revenue } )
+      respond_with( { revenue: merchant.revenue } )
     else
-      respond_with( { revenue: Merchant.find(params[:merchant_id]).revenue_by_date(params[:date]) } )
+      respond_with( { revenue: merchant.revenue_by_date(params[:date]) } )
     end
   end
 end
