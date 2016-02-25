72afe0d47141dfd4fef8f7cf4c30818e4266a8b7
Beth Secor <elizabeth.a.secor@gmail.com>
2016-02-24 17:31:13 -0700

    Most items endpoint complete

--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,7 @@ Rails.application.routes.draw do
         get 'find_all', to: 'find#index'
         get 'random', to: 'random#show'
         get '/most_revenue', to: 'revenues#index'
+        get '/most_items', to: 'most_items#index'
       end
       resources :items, only: [:show, :index] do
         get '/invoice_items', to: 'items/invoice_items#index'
